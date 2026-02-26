2f311c411a525c840acb6a185c0990777916ead2
pedro.minella1 <pedro.minella1@gmail.com>
2026-02-26 08:47:13 -0300

    dataframe.df

---
+++ aula 2.py
@@ -0,0 +1,8 @@
+import pandas as pd
+df = pd.read_csv(""C:\\Users\\pedro\\OneDrive\\Documentos\\analise de dados\\notas.csv"")
+df.shape
+df.columns
+df.dtypes
+df.insa().sum()
+df.head()
+df.tail()
